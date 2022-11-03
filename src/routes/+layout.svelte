c641876ef87b6f1f49858d26e29b31773167716a
PalaBeaveR <PalaBeaveR@gmail.com>
2022-11-03 21:58:18 +0200

    first

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,55 @@
+<style>
+
+    :root {
+        --header-color: purple;
+        --nav-color: #d2691e;
+        --footer-color: green;
+    }
+
+    header {
+        background-color: var(--header-color);
+        box-shadow: 0 3px 5px rgba(0, 0, 0, 0.4);
+    }
+
+    header > .logo {
+        margin: 0;
+        padding: 1vw;
+        color: white;
+    }
+
+    nav {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+    }
+
+    nav > a {
+        margin-top: 1vw;
+        padding: 1vw 2vw;
+        background: var(--nav-color);
+        font-size: 2em;
+        text-decoration: none;
+        color: white; 
+        border-radius: 2vw;
+    }
+
+    nav > a:not(:first-child) {
+        margin-left: 5vw;
+    }
+
+    footer {
+        position: fixed;
+        width: 100vw;
+        background-color: var(--footer-color);
+        text-align: center;
+        color: white;
+        padding: 2vh 0;
+        bottom: 0;
+    }
+</style>
+
+<header><h1 class="logo">Idk Blog</h1></header>
+<nav><a href="\">Posts</a><a href="">Placeholder 1</a><a href="">Placeholder 2</a></nav>
+<slot />
+
+<footer>PalaBeaveR ©</footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,61 @@
+<style>
+    .showcase {
+        width: 100%;
+        display: flex;
+        flex-wrap: wrap;
+        padding: 0 10vw;
+        justify-content: space-around;
+    }
+
+    .post {
+        width: 30%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+    .post > img {
+        max-width: 100%;
+    }
+
+    .post > h2 {
+        margin: 0;
+    }
+
+    .post > p {
+        margin: 0;
+    }
+</style>
+
+<div class="showcase">
+    <a href="post" class="post">
+        <img src="post.gif" alt="">
+        <h2 class="title">Post Title</h2>
+        <p class="description">Post Description</p>
+    </a>
+    <a href="post" class="post">
+        <img src="post.gif" alt="">
+        <h2 class="title">Post Title</h2>
+        <p class="description">Post Description</p>
+    </a>
+    <a href="post" class="post">
+        <img src="post.gif" alt="">
+        <h2 class="title">Post Title</h2>
+        <p class="description">Post Description</p>
+    </a>
+    <a href="post" class="post">
+        <img src="post.gif" alt="">
+        <h2 class="title">Post Title</h2>
+        <p class="description">Post Description</p>
+    </a>
+    <a href="post" class="post">
+        <img src="post.gif" alt="">
+        <h2 class="title">Post Title</h2>
+        <p class="description">Post Description</p>
+    </a>
+    <a href="post" class="post">
+        <img src="post.gif" alt="">
+        <h2 class="title">Post Title</h2>
+        <p class="description">Post Description</p>
+    </a>
+</div>
+
